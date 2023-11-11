2cf335af61bc1dba7fb5fdfcd8c99b689906255a
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-11-11 18:09:54 +0300

    добавлена вёрстка под десктопы

--- src/components/MainForm.vue
+++ src/components/MainForm.vue
@@ -1,108 +1,114 @@
 <template>
   <form class="form">
-    <h2 class="form__title">Основная информация</h2>
-    <div class="form__part">
-      <label class="form__field">
-        <h4 class="form__field-title form__field-title_mandatory">Фамилия</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title form__field-title_mandatory">Имя</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Отчество</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title form__field-title_mandatory">Дата рождения</h4>
-        <input class="form__input" type="date">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title form__field-title_mandatory">Номер телефона</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Пол</h4>
-        <div class="form__field_radio">
-          <div class="">
-            <input class="form__radio" type="radio" name="gender"> <p>Мужчина</p>
+    <div class="form__wrapper">
+      <h2 class="form__title">Основная информация</h2>
+      <div class="form__part">
+        <label class="form__field">
+          <h4 class="form__field-title form__field-title_mandatory">Фамилия</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title form__field-title_mandatory">Имя</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Отчество</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title form__field-title_mandatory">Дата рождения</h4>
+          <input class="form__input" type="date">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title form__field-title_mandatory">Номер телефона</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Пол</h4>
+          <div class="form__field_radio">
+            <div class="">
+              <input class="form__radio" type="radio" name="gender"> <p>Мужчина</p>
+            </div>
+            <div class="">
+              <input class="form__radio" type="radio" name="gender"> <p>Женщина</p>
+            </div>
           </div>
-          <div class="">
-            <input class="form__radio" type="radio" name="gender"> <p>Женщина</p>
-          </div>
-        </div>
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title form__field-title_mandatory">Группа клиентов</h4>
-        <select class="form__select" name="" id="" multiple>
-          <option value="">VIP</option>
-          <option value="">Проблемные</option>
-          <option value="">ОМС</option>
-        </select>
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Лечащий врач</h4>
-        <select class="form__select" name="" id="">
-          <option value="">Иванов</option>
-          <option value="">Захаров</option>
-          <option value="">Чернышева</option>
-        </select>
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Не отправлять СМС</h4>
-        <input class="form__check" type="checkbox">
-      </label>
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title form__field-title_mandatory">Группа клиентов</h4>
+          <select class="form__select" name="" id="" multiple>
+            <option value="">VIP</option>
+            <option value="">Проблемные</option>
+            <option value="">ОМС</option>
+          </select>
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Лечащий врач</h4>
+          <select class="form__select" name="" id="">
+            <option value="">Иванов</option>
+            <option value="">Захаров</option>
+            <option value="">Чернышева</option>
+          </select>
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Не отправлять СМС</h4>
+          <input class="form__check" type="checkbox">
+        </label>
+      </div>
     </div>
-    <h2 class="form__title">Адрес</h2>
-    <div class="form__part">
-      <label class="form__field">
-        <h4 class="form__field-title">Индекс</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Страна</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Область</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title form__field-title_mandatory">Город</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Улица</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Дом</h4>
-        <input class="form__input" type="text">
-      </label>
+    <div class="form__wrapper">
+      <h2 class="form__title">Адрес</h2>
+      <div class="form__part">
+        <label class="form__field">
+          <h4 class="form__field-title">Индекс</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Страна</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Область</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title form__field-title_mandatory">Город</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Улица</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Дом</h4>
+          <input class="form__input" type="text">
+        </label>
+      </div>
     </div>
-    <h2 class="form__title">Документы</h2>
-    <div class="form__part">
-      <label class="form__field">
-        <h4 class="form__field-title form__field-title_mandatory">Тип документа</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Серия</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Номер</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title">Кем выдан</h4>
-        <input class="form__input" type="text">
-      </label>
-      <label class="form__field">
-        <h4 class="form__field-title form__field-title_mandatory">Дата выдачи</h4>
-        <input class="form__input" type="text">
-      </label>
+    <div class="form__wrapper">
+      <h2 class="form__title">Документы</h2>
+      <div class="form__part">
+        <label class="form__field">
+          <h4 class="form__field-title form__field-title_mandatory">Тип документа</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Серия</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Номер</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title">Кем выдан</h4>
+          <input class="form__input" type="text">
+        </label>
+        <label class="form__field">
+          <h4 class="form__field-title form__field-title_mandatory">Дата выдачи</h4>
+          <input class="form__input" type="text">
+        </label>
+      </div>
     </div>
     <button class="button form__button">Создать</button>
   </form>
@@ -185,6 +191,45 @@ export default {
     &__button {
       min-width: 13rem;
       margin-top: 0.5rem;
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    .form {
+      min-width: 58rem;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      align-items: flex-start;
+      position: relative;
+      padding-bottom: 0;
+
+      &__wrapper {
+        @include vertical-layout;
+      }
+
+      &__check {
+        cursor: pointer;
+      }
+  
+      &__select {
+        cursor: pointer;
+      }
+
+      &__radio {
+        cursor: pointer;
+      }
+
+      &__button {
+        position: absolute;
+        min-width: 18rem;
+        border-radius: 1.5rem;
+        font-size: 3rem;
+        padding: 2rem;
+        bottom: 6rem;
+        left: 50%;
+        cursor: pointer;
+        margin: 0;
+      }
     }
   }
 </style>
